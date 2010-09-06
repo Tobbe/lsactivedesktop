3b359c5552f86858b6e654fc9a687bbd5c75b5ec
Tobbe Lundberg <tobbesweden@gmail.com>
2010-09-06 17:28:11 -0400

    Added url encoding too, just because the code was already there on the same page I found the decoding on.

--- urlcode.cpp
+++ urlcode.cpp
@@ -5,6 +5,12 @@ char UrlCode::FromHex(char ch)
 	return isdigit(ch) ? ch - '0' : tolower(ch) - 'a' + 10;
 }
 
+char UrlCode::ToHex(char code)
+{
+	static char hex[] = "0123456789abcdef";
+	return hex[code & 15];
+}
+
 std::string UrlCode::Decode(std::string url)
 {
 	const char *pstr = url.c_str();
@@ -31,4 +37,30 @@ std::string UrlCode::Decode(std::string url)
 	delete buf;
 
 	return decoded;
+}
+
+std::string UrlCode::Encode(std::string url)
+{
+	const char *pstr = url.c_str();
+	char *buf = new char[url.length() * 3 + 1];
+	char *pbuf = buf;
+
+	while (*pstr) {
+		if (isalnum(*pstr) || *pstr == '-' || *pstr == '_' || *pstr == '.' || *pstr == '~') {
+			*pbuf++ = *pstr;
+		} else if (*pstr == ' ') {
+			*pbuf++ = '+';
+		} else {
+			*pbuf++ = '%', *pbuf++ = ToHex(*pstr >> 4), *pbuf++ = ToHex(*pstr & 15);
+		}
+
+		pstr++;
+	}
+
+	*pbuf = '\0';
+
+	std::string encoded = buf;
+	delete buf;
+
+	return encoded;
 }
